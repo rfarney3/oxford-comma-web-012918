45d09e41f15140edcc62652f77f64b780651e7eb
Ryan Farney <ryanfarney@Ryans-MacBook-Pro-104.local>
2018-01-11 16:14:17 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+	str = ''
+	array.each do |el|
+		if array.length == 1
+			str += el
+		elsif array.length == 2 && el == array[-1]
+			str += ("#{el}")
+		elsif array.length == 2
+			str += ("#{el} and ")
+		elsif array.length > 2 && el == array[-2]
+			str += "#{el}, and "
+		elsif array.length > 2 && el == array[-1]
+			str += "#{el}"
+		else
+			str += "#{el}, "
+		end
+	end
+	str
 end
